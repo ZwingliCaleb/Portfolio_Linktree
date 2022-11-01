b2a8915faaa4e2b9e5b98f4397dd1b0bb9efa81a
ZwingliCaleb <calebzwingli@gmail.com>
2022-11-01 19:09:25 +0300

    Added two js files and a few PNG images to be used in the project

--- src/components/LinkButtons.js
+++ src/components/LinkButtons.js
@@ -8,12 +8,29 @@ const LinkButtons=props=>{
         link
     } = props
 
+    const myStyles = ({
+        myMargin: {
+            marginBottom: "20px"
+        },
+        btnText: {
+            fontFamily: "Monaco",
+            margin: "10px",
+            fontSize: "15px",
+            color: "white",
+            // textShadow: "1px 1px 4px #000000"
+        },
+        btn: {
+            padding: "24px, 32px, 24px, 32px",
+            width: "1152px",
+            height: "76px",
+            radius: "8px"
+        }
+    })
+
     return(
-        <div>
-            <h3>{title}</h3>
-            <Button color="danger"class="btn btn-outline-danger"outline href={link}>
-                {name}
-            </Button>
+        <div style={myStyles.myMargin}>
+            <h3 style={myStyles.btnText}>{title}</h3>
+            <Button style={myStyles.btn} color="danger"class="btn btn-outline-danger"outline href={link}>{name}</Button>
         </div>
 
     )
